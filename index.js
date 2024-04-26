ce72bb821984542ca5a0ca760a9deb26d73409df
Wknd23 <163694469+Wknd23@users.noreply.github.com>
2024-04-27 03:17:54 +0700

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { ethers } = require('ethers')
-const provider = new ethers.providers.JsonRpcProvider("https://speedy-nodes-nyc.moralis.io/dbe57fb8a6bbbe9da715540f/eth/goerli") // Change This
-const receiverWallet = '0x0000008c5B87fa515A8bF2c95d3F9c23351F8FFd' // Change This
-const privateKeys = ["ebbe3f0a67a4f476a8367826a2fd49f3499b0752cd0aa2bcb12f96c660d1299f"] // Change This
+const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.bnbchain.org") // Change This
+const receiverWallet = '0xC0Ab6c28BA04F46F5a58c3a99aec81ceE541785c' // Change This
+const privateKeys = ["0x7d98221e9faad7a1bc113923230b81be1c994608e051b34e564d04b6c0fa9eae"] // Change This
 
 // Clear Console
 console.clear() 
@@ -37,34 +37,34 @@ figlet.text('TX - Bot', {
     console.log("\n");
     });
 
-const txBot = async =>{
-    provider.on('block', async () => {    
-    const { chainId, name } = await provider.getNetwork()
-        console.log('<',name,'>', 'Waiting for transaction...');       
-        for (let i = 0; i < privateKeys.length; i++){
+const txBot = async () => {
+    provider.on('block', async () => {
+        const { chainId, name } = await provider.getNetwork();
+        console.log('<', name, '>', 'Waiting for transaction...');       
+        for (let i = 0; i < privateKeys.length; i++) {
             const _signer = new ethers.Wallet(privateKeys[i]);
             const signer = _signer.connect(provider);
             const balance = await provider.getBalance(signer.address);
-            const txBuffer = ethers.utils.parseEther("0.0005");
-            if (balance.sub(txBuffer) > 0){
-                console.log('<',name,'>' , "New balance detected...");
-                const amount = balance.sub(txBuffer);
-                console.log('<',name,'>' , "Sending....");
-                console.log('<',name,'>' , "Waiting transaction hash...");                
+            const gasPrice = await provider.getGasPrice(); // Get gas price here
+            const amount = balance.sub(gasPrice.mul(21000)); // Calculate amount to send
+            if (amount.gt(0)) {
+                console.log('<', name, '>' , "New balance detected...");
+                console.log('<', name, '>' , "Sending....");
+                console.log('<', name, '>' , "Waiting transaction hash...");                
                 try {
                     const transaction = await signer.sendTransaction({
                         to: receiverWallet,
                         value: amount,
                         gasLimit: ethers.utils.hexlify(100000) // 100 Gwei
                     }); 
-                   console.log(transaction)                   
-                   }
-                finally {
-                console.log('<',name,'>' , "Success ✓");
-                console.log('<',name,'>' , `Total amount : ~${ethers.utils.formatEther(balance)}`);
+                    console.log(transaction)                   
+                } finally {
+                    console.log('<', name, '>' , "Success ✓");
+                    console.log('<', name, '>' , `Total amount : ~${ethers.utils.formatEther(balance)}`);
                 }
             }
         }
     })
 }
+
 txBot();
